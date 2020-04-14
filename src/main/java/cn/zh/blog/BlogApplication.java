7dc10b620690d83a30c4b1bccb3f0173b7159acf
ZH <ZH@zh.com>
2020-04-14 23:01:46 +0800

    第一次提交

---
+++ src/main/java/cn/zh/blog/BlogApplication.java
@@ -0,0 +1,16 @@
+package cn.zh.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**@author 鄭豪
+ * 启动引导类
+ */
+@SpringBootApplication
+public class BlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogApplication.class, args);
+    }
+
+}
